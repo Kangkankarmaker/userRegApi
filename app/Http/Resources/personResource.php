7d537ae3ed6658d5618e3ec861a39cae13e8958d
kangkankarmaker <kangkankarmaker00@gmail.com>
2020-11-02 17:56:44 +0600

    fix some error

--- app/Http/Resources/personResource.php
+++ app/Http/Resources/personResource.php
@@ -14,6 +14,11 @@ class personResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return [
+            'id'=>$this ->id,
+            'user_email'=>$this ->user_email,
+            'created_at'=>$this ->created_at
+        ];
     }
 }
--- app/Http/Resources/personResourceCollection.php
+++ app/Http/Resources/personResourceCollection.php
@@ -15,5 +15,6 @@ class personResourceCollection extends ResourceCollection
     public function toArray($request)
     {
         return parent::toArray($request);
+
     }
 }
